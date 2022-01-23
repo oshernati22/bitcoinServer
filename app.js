66363b74a6799b7d13a95efef782e15bbbefb739
Osher <oshernati22@gmail.com>
2022-01-23 18:00:39 +0200

    add compression

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const compression = require('compression');
 
 const AddressesRouter = require('./routes/addressesRoutes');
 
 const app = express();
-
+app.use(compression());
 app.use(express.json());
 
 app.use(morgan('dev'));
