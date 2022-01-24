ae6a6176233357a3f548357d7fd999748edcad8f
Osher <oshernati22@gmail.com>
2022-01-24 18:14:38 +0200

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 // const morgan = require('morgan');
 const compression = require('compression');
+const cors = require('cors');
 
 const AddressesRouter = require('./routes/addressesRoutes');
 //midleware
 const app = express();
+app.use(cors());
 app.use(compression()); //compress json texts in the production.
 app.use(express.json());
 // app.use(morgan('dev'));
